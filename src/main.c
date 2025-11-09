618abd96b5f592ae5dce25ef3dd9eeb2ddfd2713
Amos Shehzad <lukkygill26@gmail.com>
2025-11-09 20:59:18 +0000

    feat(feature-3): add command history and re-execution of commands

--- src/main.c
+++ src/main.c
@@ -7,16 +7,30 @@ int main(void) {
     char** argv;
 
     while ((cmdline = get_command(PROMPT, stdin)) != NULL) {
+        // Handle !n (re-execute from history)
+        if (cmdline[0] == '!' && strlen(cmdline) > 1) {
+            int index = atoi(cmdline + 1);
+            char* recalled = get_saved_command(index);
+            if (recalled) {
+                printf("Re-executing: %s\n", recalled);
+                free(cmdline);
+                cmdline = strdup(recalled);
+            } else {
+                printf("No such command in history.\n");
+                free(cmdline);
+                continue;
+            }
+        }
+
         if ((argv = parse_input(cmdline)) != NULL) {
-            // Check if built-in command
             if (!handle_builtin(argv))
                 execute_command(argv);
 
-            // Free memory
             for (int i = 0; argv[i] != NULL; i++)
                 free(argv[i]);
             free(argv);
         }
+
         free(cmdline);
     }
 
